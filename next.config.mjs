93dda68105ca8aa296ac0effa85bb02d081bb752
VRAJ K PATEL <99316553+Itsvrajpatel@users.noreply.github.com>
2026-07-11 14:06:10 +0530

    feat: solved build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const backendUrl =
+  process.env.BACKEND_URL || "http://localhost:8000";
+
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.BACKEND_URL}/api/:path*`,
+        destination: `${backendUrl}/api/:path*`,
       },
     ];
   },
