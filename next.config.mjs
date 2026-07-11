7258a2604a108ab3835256b2d46e7ac7204b75b5
VRAJ K PATEL <99316553+Itsvrajpatel@users.noreply.github.com>
2026-07-11 13:49:42 +0530

    feat: changed in nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,11 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        source: "/api/:path*",
+        destination: `${process.env.BACKEND_URL}/api/:path*`,
       },
     ];
   },
 };
+
 export default nextConfig;
